8770e41cef5277f39c57c93e212210e486c03706
noahblob <noahhagardent@gmail.com>
2023-07-06 13:49:25 +1200

    Added javascript for contact form submission

--- send-email.php
+++ send-email.php
@@ -1,46 +1,43 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
-require 'vendor/autoload.php';
+require "vendor/autoload.php";
 
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-//Create an instance; passing `true` enables exceptions
+$name = htmlspecialchars($name);
+$email = htmlspecialchars($email);
+$message = htmlspecialchars($message);
+
+
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER; 
     $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'noahhagardent@gmail.com';
-    $mail->Password   = 'polimunrzrpkoyhe';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMSSL;
-    $mail->Port       = 465;
+    $mail->SMTPAuth = true;
 
-    //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    $mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Username = "noahhagardent@gmail.com";
+    $mail->Password = "polimunrzrpkoyhe";
+
+    $mail->setFrom($email, $name);
+    $mail->addAddress('noahhagardent@gmail.com', 'Noah');
+
+    $mail->isHTML(true);
+    $mail->Subject = 'New Message From Portfolio';
+    $mail->Body = $message;
 
     $mail->send();
-    echo 'Message has been sent';
+    header("Location: index.html");
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+?>
