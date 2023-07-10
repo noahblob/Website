f8ad442cb510fdbe4e544000dabaf4554c62e21f
noahblob <noahhagardent@gmail.com>
2023-07-10 21:53:40 +1200

    Added some animation for page contents

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-// JS for hiding navbar on scroll down and showing on scroll up
-
+// Hide navbar on scroll down and show on scroll up
 const nav = document.querySelector(".navbar");
 let lastScrollY = window.scrollY;
 
 window.addEventListener("scroll", () => {
-    const scrollY = window.scrollY;
-    if (scrollY > lastScrollY) {
-        nav.classList.add("navbar--hidden");
+  const scrollY = window.scrollY;
+  if (scrollY > lastScrollY) {
+    nav.classList.add("navbar--hidden");
+  } else {
+    nav.classList.remove("navbar--hidden");
+  }
+  lastScrollY = scrollY;
+});
+
+// Anchor links smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  })
+});
+
+// Reveal on scroll
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
     } else {
-        nav.classList.remove("navbar--hidden");
+      reveals[i].classList.remove("active");
     }
-    lastScrollY = scrollY;
-});
+  }
+}
 
-// JS for anchor links smooth scroll
+window.addEventListener("scroll", reveal);
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => { 
-    anchor.addEventListener("click", function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth"
-        });
-    })
-});
+// To check the scroll position on page load
+reveal();
